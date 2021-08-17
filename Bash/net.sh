40151cc3bff9a29db6fd7cdcdb6a65ac7ca43083
NetFlex <84316160+Geruays@users.noreply.github.com>
2021-08-17 09:42:35 +0700

    Create net.sh

---
+++ Bash/net.sh
@@ -0,0 +1,18 @@
+echo 'By NetFlex'
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install apt-transport-https -y
+sudo apt-get install ca-certificates -y
+sudo apt-get install curl -y
+sudo apt-get install lsb-release -y
+sudo apt-get install ca-certificates -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok.zip
+unzip ngrok.zip
